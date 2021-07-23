3bf2dec2d20e7e6c00a046744e9850c6426386a8
Muksam212 <muksamthebe10@gmail.com>
2021-07-23 15:16:38 +0545

    first commit

---
+++ snake.py
@@ -0,0 +1,42 @@
+from turtle import *
+from random import randrange
+from freegames import square, vector
+
+food=vector(0,0)
+snake=[vector(10,0)]
+aim=vector(0,-10)
+
+def change(x,y):
+    #to change the position
+    aim.x=x
+    aim.y=y
+
+def move():
+    head=snake[-1].copy()
+    head.move(aim)
+    snake.append(head)
+
+    if head == food:
+        print("Len of snake:", len(snake))
+        food.x=randrange(-15,15)*10
+        food.y=randrange(-15,15)*10
+    else:
+        snake.pop(0)
+    clear()
+
+    for body in snake:
+        square(body.x, body.y, 9,'green')
+    square(food.x, food.y, 9,'red')
+    ontimer(move, 100)
+    update()
+
+setup(420,420,370,0)
+hideturtle()
+tracer(False)
+listen()
+onkey(lambda: change(10,0),'Right')
+onkey(lambda: change(-10,0),'Left')
+onkey(lambda: change(0,10),'Up')
+onkey(lambda: change(0,-10),'Down')
+move()
+done()
